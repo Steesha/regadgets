335229bf1555eb01c2e8ef41f5b7b3f013ccd0b5
steesha <jsteesha@gmail.com>
2024-09-22 15:27:23 +0800

    add bxorl

--- crypto/__init__.py
+++ crypto/__init__.py
@@ -1,5 +1,5 @@
 from .rc4 import rc4_crypt, rc4_init, rc4_keystream
-from .bxors import bxorr_dec, bxorr_enc, bxor
+from .bxors import bxorr_dec, bxorr_enc, bxorl_dec, bxor, bxorl_enc
 from .xxtea import xxtea_decrypt, xxtea_encrypt, xxtea_ciscn2024_shift, xxtea_std_shift
 from .xtea import xtea_decrypt, xtea_encrypt
 from .tea import tea_decrypt, tea_encrypt
--- crypto/bxors.py
+++ crypto/bxors.py
@@ -11,6 +11,11 @@ def bxorr_dec(encrypted: bytes) -> bytes:
         result.append(i)
     return bytes(result[::-1])
 
+# From: x_0 , x_1 ^ x_0, x_2 ^ x_1, ..... , x_(n-1) ^ x_n
+def bxorl_dec(encrypted: bytes) -> bytes:
+    encrypted = encrypted[::-1]
+    return bxorr_dec(encrypted)[::-1]
+
 # NOTE:
 # bxorr_dec(bxorr_enc(x)) == bxorr_enc(bxorr_dec(x)) == x
 # To: x_0 ^ x_1, x_1 ^ x_2, x_2 ^ x_3, ..... , x_(n-1) ^ x_n, x_n
@@ -21,6 +26,10 @@ def bxorr_enc(raw: bytes) -> bytes:
         result.append(raw[i] ^ raw[i + 1])
     result.append(raw[-1])
     return bytes(result)
+
+# To: x_0 , x_1 ^ x_0, x_2 ^ x_1, ..... , x_(n-1) ^ x_n
+def bxorl_enc(encrypted: bytes) -> bytes:
+    return bxorr_enc(encrypted[::-1])[::-1]
 
 def bxor(data1: bytes | list, data2: bytes | list) -> bytes:
     if len(data1) != len(data2):
