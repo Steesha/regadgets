2d9f94a63a9c6df0e0d2701e30d6e9731e702d08
steesha <jsteesha@gmail.com>
2024-09-27 15:47:28 +0800

    fix sbox_transform

--- trans/sbox.py
+++ trans/sbox.py
@@ -11,6 +11,6 @@ def generate_sbox(_from: bytes | List[int], _to: bytes | List[int]) -> Tuple[Lis
 def sbox_transform(_from: bytes | List[int], box: List[int]):
     r = [_from[box[i]] for i in range(len(_from))]
     if type(_from) == bytes:
-        return bytes(_from)
+        return bytes(r)
     else:
-        return _from
+        return r
