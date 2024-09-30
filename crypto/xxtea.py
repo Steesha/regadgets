4d5d8933de69dbcd8eb980861af8ec6ba8a531b2
jsteesha <jsteesha@gmail.com>
2024-09-30 12:27:22 +0800

    xxtea

--- crypto/xxtea.py
+++ crypto/xxtea.py
@@ -1,49 +1,63 @@
 from typing import List
 from struct import pack
 from ctypes import c_uint32
+from ctypes import c_uint64
 
-def print_shift_debug(z, y, sum, k, p, e, result):
-    print('----------------------XXTEA-DEBUG----------------------')
-    print('z', z.value, hex(z.value))
-    print('y', y.value, hex(y.value))
-    print('sum', sum.value, hex(sum.value))
-    print('p', p, hex(p))
-    print('e', e.value, hex(e.value))
-    print('z >> 5', (z.value >> 5) & 0xffffffff, hex((z.value >> 5) & 0xffffffff))
-    print('y << 2', (y.value << 2) & 0xffffffff, hex((y.value << 2) & 0xffffffff))
-    print('y >> 3', (y.value >> 3) & 0xffffffff, hex((y.value >> 3) & 0xffffffff))
-    print('z << 4', (z.value << 4) & 0xffffffff, hex((z.value << 4) & 0xffffffff))
-    print('sum ^ y', '   ', (sum.value ^ y.value) & 0xffffffff, hex((sum.value ^ y.value) & 0xffffffff))
-    print('(p&3)^e', '   ', k[(p & 3) ^ e.value].value & 0xffffffff, hex(k[(p & 3) ^ e.value].value & 0xffffffff))
-    print('k[(p&3)^e] ^ z', '                        ', k[(p & 3) ^ e.value].value ^ z.value, hex(k[(p & 3) ^ e.value].value ^ z.value))
-    print('$1 = [(z>>5)^(y<<2)]+[(y>>3)^(z<<4)]', '  ',((((z.value >> 5) ^ (y.value << 2)) + ((y.value >> 3) ^ (z.value << 4)))) & 0xffffffff,
-          hex((((z.value >> 5) ^ (y.value << 2)) + ((y.value >> 3) ^ (z.value << 4))) & 0xffffffff))
-    print('$2 = (sum^y)+(k[(p&3)^e]^z)', '           ', ((sum.value ^ y.value) + (k[(p & 3) ^ e.value].value ^ z.value)) & 0xffffffff, 
-          hex(((sum.value ^ y.value) + (k[(p & 3) ^ e.value].value ^ z.value)) & 0xffffffff))
-    print('result = $1 ^ $2', '                      ', result & 0xffffffff, hex(result & 0xffffffff))
-    print('----------------------XXTEA-DEBUG----------------------')
+def xxtea_std_shift(z, y, sum, k, p, debug = False):
+    if debug:
+        print("-----------------XXTEA_STD_SHIFT-----------------")
+    e = (sum.value >> 2) & 3
+    PE = (p & 3) ^ e
+    Ly = y.value << 2
+    Ry = y.value >> 3
+    Lz = z.value << 4
+    Rz = z.value >> 5 
 
-
-def xxtea_std_shift(z, y, sum, k, p, e):
-    result = (((z.value >> 5) ^ (y.value << 2)) + ((y.value >> 3) ^ (z.value << 4))) ^ (
-        (sum.value ^ y.value) + (k[(p & 3) ^ e.value].value ^ z.value)
-    )
+    LzRy = Rz ^ Ly
+    LyRz = Ry ^ Lz
+    SY = sum.value ^ y.value
+    K = k[PE].value
+    KZ = K ^ z.value
+    result = (LzRy + LyRz) ^ (KZ + SY)
+    if debug:
+        print("sum = ", hex(sum.value))
+        print("e   = ", hex(e))
+        print("PE  = ", hex(PE))
+        print("z   = ", hex(z.value))
+        print("y   = ", hex(y.value))
+        print("Ly  = ", hex(Ly))
+        print("Ry  = ", hex(Ry))
+        print("Lz  = ", hex(Lz))
+        print("Rz  = ", hex(Rz))
+        print("LzRy= ", hex(LzRy))
+        print("LyRz= ", hex(LyRz))
+        print("K   = ", hex(K))
+        print("KZ  = ", hex(KZ))
+        print("-----------------XXTEA_STD_SHIFT-----------------")
     return result
 
+def xxtea_ciscn2024_shift(z, y, sum, k, p, debug = False):
+    e = (sum.value >> 2) & 3
+    PE = (p & 3) ^ e
+    Ly = y.value << 2
+    Ry = y.value >> 3
+    Lz = z.value << 4
+    Rz = z.value >> 5 
 
-def xxtea_ciscn2024_shift(z, y, sum, k, p, e):
-    result = (
-        (((z.value >> 5) ^ (y.value << 2)) + ((y.value >> 3) ^ (z.value << 4)))
-        ^ ((sum.value ^ y.value))
-    ) + (k[(p & 3) ^ e.value].value ^ z.value)
+    LzRy = Rz ^ Ly
+    LyRz = Ry ^ Lz
+    SY = sum.value ^ y.value
+    K = k[PE].value
+    KZ = K ^ z.value
+    result = ((LzRy + LyRz) ^ SY) + KZ
     return result
-
 
 def xxtea_encrypt(
     src: List[int],
     key: List[int],
     delta: int = 0x9E3779B9,
-    additional_rounds: int = 0,
+    round_base: int = 6,
+    round_addi: int = 52,
     shift_func=xxtea_std_shift,
     debug=False,
 ):
@@ -52,28 +66,19 @@ def xxtea_encrypt(
     sum, e = c_uint32(0), c_uint32(0)
     delta = c_uint32(delta)
     n = len(src)
-    rounds = 6 + 52 // n + additional_rounds
+    rounds = round_base + round_addi // n
     z = src[n - 1]
     for _ in range(rounds):
         sum.value += delta.value
-        e.value = (sum.value >> 2) & 3
         for p in range(n - 1):
             y = src[p + 1]
-            shift_result = shift_func(z, y, sum, key, p, e)
+            shift_result = shift_func(z, y, sum, key, p, debug)
             src[p].value += shift_result
-            ###
-            if debug:
-                print_shift_debug(z, y, sum, key, p, e, shift_result)
-            ###
             z = src[p]
         p += 1
         y = src[0]
-        shift_result = shift_func(z, y, sum, key, p, e)
+        shift_result = shift_func(z, y, sum, key, p, debug)
         src[n - 1].value += shift_result
-        ###
-        if debug:
-            print_shift_debug(z, y, sum, key, p, e, shift_result)
-        ###
         z = src[n - 1]
     return [i.value for i in src]
 
@@ -84,7 +89,8 @@ def xxtea_decrypt(
     src: List[int],
     key: List[int],
     delta: int = 0x9E3779B9,
-    additional_rounds: int = 0,
+    round_base: int = 6,
+    round_addi: int = 52,
     shift_func=xxtea_std_shift,
     debug=False,
 ):
@@ -93,28 +99,19 @@ def xxtea_decrypt(
     sum, e, y = c_uint32(0), c_uint32(0), c_uint32(0)
     delta = c_uint32(delta)
     n = len(src)
-    rounds = 6 + 52 // n + additional_rounds
+    rounds = round_base + round_addi // n
     sum.value = rounds * delta.value
     y = src[0]
     for _ in range(rounds):
-        e.value = (sum.value >> 2) & 3
         for p in range(n - 1, 0, -1):
             z = src[p - 1]
-            shift_result = shift_func(z, y, sum, key, p, e)
+            shift_result = shift_func(z, y, sum, key, p, debug)
             src[p].value -= shift_result 
-            ###
-            if debug:
-                print_shift_debug(z, y, sum, key, p, e, shift_result)
-            ###
             y = src[p]
         p -= 1
         z = src[n - 1]
-        shift_result = shift_func(z, y, sum, key, p, e)
+        shift_result = shift_func(z, y, sum, key, p, debug)
         src[0].value -= shift_result
-        ###
-        if debug:
-            print_shift_debug(z, y, sum, key, p, e, shift_result)
-        ###
         y = src[0]
         sum.value -= delta.value
     return [i.value for i in src]
