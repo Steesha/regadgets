72d982efe8f98d15069629de91d8ae94eeccecba
steesha <jsteesha@gmail.com>
2024-10-23 19:04:24 +0800

    Add SM4 For Regadgets.

--- crypto/__init__.py
+++ crypto/__init__.py
@@ -5,3 +5,9 @@ from .xtea import xtea_decrypt, xtea_encrypt
 from .tea import tea_decrypt, tea_encrypt
 from .aes import AES, AES_cbc_decrypt, AES_cbc_encrypt, AES_ecb_decrypt, AES_ecb_encrypt
 from .blowfish import BlowFish
+from .sm4 import encrypt as SM4_encrypt
+from .sm4 import decrypt as SM4_decrypt
+from .sm4 import decrypt_cbc as SM4_decrypt_cbc
+from .sm4 import decrypt_ecb as SM4_decrypt_ecb
+from .sm4 import encrypt_cbc as SM4_encrypt_cbc
+from .sm4 import encrypt_ecb as SM4_encrypt_ecb
